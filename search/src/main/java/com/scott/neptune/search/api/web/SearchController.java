858a6663c439bb6396a451096c66fc064cb4555a
Scott Lau <liuweigeek@163.com>
2019-10-22 22:55:50 +0800

    add: build docker image by maven

--- search/src/main/java/com/scott/neptune/search/api/web/SearchController.java
+++ search/src/main/java/com/scott/neptune/search/api/web/SearchController.java
@@ -3,7 +3,6 @@ package com.scott.neptune.search.api.web;
 import com.google.common.collect.Lists;
 import com.scott.neptune.common.response.ServerResponse;
 import com.scott.neptune.postapi.dto.PostDto;
-import com.scott.neptune.search.dto.SearchResultMap;
 import com.scott.neptune.search.remote.client.PostClient;
 import com.scott.neptune.search.remote.client.UserClient;
 import com.scott.neptune.userapi.dto.UserDto;
@@ -11,7 +10,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: scott
@@ -43,23 +43,18 @@ public class SearchController {
     @ApiOperation(value = "搜索用户和推文")
     @ApiImplicitParam(name = "keyword", value = "关键字", required = true, dataType = "string", paramType = "path")
     @GetMapping(value = "/{keyword}")
-    public ServerResponse<SearchResultMap> searchByKeyword(@PathVariable("keyword") String keyword) {
-        SearchResultMap searchResultMap = new SearchResultMap();
+    public ServerResponse<Map<String, ServerResponse>> searchByKeyword(@PathVariable("keyword") String keyword) {
 
         List<String> errorMessage = Lists.newArrayListWithExpectedSize(2);
         ServerResponse<List<UserDto>> userResponse = userClient.findByKeyword(keyword);
-        if (userResponse.isFailed()) {
-            errorMessage.add(userResponse.getMsg());
-        } else {
-            searchResultMap.setUserList(userResponse.getData());
-        }
         ServerResponse<List<PostDto>> postResponse = postClient.findByKeyword(keyword);
-        if (postResponse.isFailed()) {
-            errorMessage.add(postResponse.getMsg());
-        } else {
-            searchResultMap.setPostList(postResponse.getData());
-        }
-        String errorMsg = CollectionUtils.isEmpty(errorMessage) ? "" : errorMessage.toString();
-        return ServerResponse.createBySuccess(errorMsg, searchResultMap);
+
+        Map<String, ServerResponse> searchResultMap = new HashMap<String, ServerResponse>() {
+            {
+                put("userList", userResponse);
+                put("postList", postResponse);
+            }
+        };
+        return ServerResponse.createBySuccess(searchResultMap);
     }
 }
