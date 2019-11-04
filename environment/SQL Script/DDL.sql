e48b5a673481c7bf616b9805fe2cd5c47a2a2961
Scott Lau <liuweigeek@163.com>
2019-11-04 21:40:39 +0800

    update: DDL

--- environment/SQL Script/DDL.sql
+++ environment/SQL Script/DDL.sql
@@ -2,38 +2,38 @@ create schema db_neptune_blog collate utf8mb4_0900_ai_ci;
 
 create table t_friend_relation
 (
-    follow_date datetime null,
+    follow_date datetime    null,
     follow_from varchar(32) null,
-    from_id varchar(32) not null,
-    to_id varchar(32) not null,
+    from_id     varchar(32) not null,
+    to_id       varchar(32) not null,
     primary key (from_id, to_id)
 );
 
 create table t_post
 (
-    id varchar(32) not null
+    id          varchar(32)   not null
         primary key,
-    content varchar(1024) null,
-    create_date datetime null,
-    device varchar(32) null,
-    update_date datetime null,
-    user_id varchar(255) null
+    content     varchar(1024) null,
+    create_date datetime      null,
+    device      varchar(32)   null,
+    update_date datetime      null,
+    user_id     varchar(255)  null
 );
 
 create table t_user
 (
-    id varchar(32) not null
+    id            varchar(32)  not null
         primary key,
-    email varchar(50) null,
-    lang_key varchar(6) null,
-    login_date datetime null,
-    password varchar(64) null,
-    nickname varchar(32) null,
-    register_date datetime null,
-    sex tinyint not null,
-    token varchar(256) null,
-    username varchar(32) null,
-    birthday datetime null,
+    email         varchar(50)  null,
+    lang_key      varchar(6)   null,
+    login_date    datetime     null,
+    password      varchar(64)  null,
+    nickname      varchar(32)  null,
+    register_date datetime     null,
+    sex           tinyint      not null,
+    token         varchar(256) null,
+    username      varchar(32)  null,
+    birthday      datetime     null,
     constraint UK_i6qjjoe560mee5ajdg7v1o6mi
         unique (email),
     constraint UK_jhib4legehrm4yscx9t3lirqi
@@ -42,9 +42,9 @@ create table t_user
 
 create table t_user_avatar
 (
-    user_id varchar(32) not null,
-    size_type tinyint(1) not null,
-    url varchar(500) null,
+    user_id   varchar(32)  not null,
+    size_type tinyint(1)   not null,
+    url       varchar(500) null,
     primary key (user_id, size_type)
 );
 
