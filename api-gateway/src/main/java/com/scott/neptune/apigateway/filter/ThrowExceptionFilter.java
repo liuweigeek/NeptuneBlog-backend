56590e21df6f65cbe96218a439727476ec76aeb2
Scott Lau <liuweigeek@outlook.com>
2020-10-20 14:10:05 +0800

    fix ZuulFilter

--- api-gateway/src/main/java/com/scott/neptune/apigateway/filter/ThrowExceptionFilter.java
+++ api-gateway/src/main/java/com/scott/neptune/apigateway/filter/ThrowExceptionFilter.java
@@ -1,11 +1,17 @@
 package com.scott.neptune.apigateway.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import com.scott.neptune.common.exception.NeptuneBlogException;
+import com.scott.neptune.common.model.ApiErrorResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author: scott
@@ -14,35 +20,45 @@ import org.springframework.stereotype.Component;
  * @Description: NeptuneBlog
  */
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class ThrowExceptionFilter extends ZuulFilter {
 
+    private static final String ERROR_STATUS_CODE_KEY = "throwable";
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public String filterType() {
-        return "post";
+        return FilterConstants.ERROR_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return -1;
+        return FilterConstants.SEND_ERROR_FILTER_ORDER - 1;
     }
 
     @Override
     public boolean shouldFilter() {
-        // only forward to errorPath if it hasn't been forwarded to already
-        return RequestContext.getCurrentContext().containsKey("error.status_code");
+        return RequestContext.getCurrentContext().containsKey(ERROR_STATUS_CODE_KEY);
     }
 
     @Override
     public Object run() {
         try {
             RequestContext ctx = RequestContext.getCurrentContext();
-            Object exception = ctx.get("error.exception");
+            Throwable exception = (Exception) ctx.get(ERROR_STATUS_CODE_KEY);
             if (exception instanceof ZuulException) {
-                ctx.remove("error.status_code");
+                ctx.remove(ERROR_STATUS_CODE_KEY);
                 ZuulException zuulException = (ZuulException) exception;
                 log.error("网关异常: ", zuulException);
-                throw new NeptuneBlogException("服务不可用，请稍后再试");
+
+                ApiErrorResponse apiErrorResponse = ApiErrorResponse.createByMessage("服务不可用，请稍后再试");
+                HttpServletResponse response = ctx.getResponse();
+                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().println(objectMapper.writeValueAsString(apiErrorResponse));
+                ctx.setSendZuulResponse(false);
             }
         } catch (Exception e) {
             log.error("拦截器异常", e);
